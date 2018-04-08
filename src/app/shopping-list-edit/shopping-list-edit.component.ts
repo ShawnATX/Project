ed71c5d85b677825bd53cd9a593edbe806d00f4c
Shawn W <37752327+ShawnATX@users.noreply.github.com>
2018-04-08 02:04:47 -0500

    Functionality to Shopping List
    Add button working

--- src/app/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ElementRef, ViewChild, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //when add button is clicked on shopping list the name and amount form values are passed into a new
+  //ingredient object and this is pushed into the ingredient list with the ingredientAdded event
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+    console.log(this);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient){
+    console.log(this);
+    this.ingredients.push(ingredient);
+  }
+
 }
